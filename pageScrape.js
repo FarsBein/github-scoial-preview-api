08c3a989658b31f379e58853291b2eaef6e2af96
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-15 14:44:36 -0400

    a wroking parsing scrpit and a home page

--- pageScrape.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-
-const fetchPage = async (url) => {
-    const page = await axios.get(url)
-    console.log(page.data)
-}
-
-fetchPage('http://127.0.0.1:5500/index.html')
